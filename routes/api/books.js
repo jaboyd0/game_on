acb0aec5bd1ebb41f665bef779654cd56bb5d95f
hafiz salman <hmsalmans1@gmail.com>
2020-07-10 17:04:01 +0500

    adding api routing

--- routes/api/books.js
+++ routes/api/books.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const booksController = require("../../controllers/booksController");
+const usersController = require("../../controllers/usersController");
 
 // Matches with "/api/books"
 router.route("/")
-  .get(booksController.findAll)
-  .post(booksController.create);
+  .get(usersController.findAll)
+  .post(usersController.create);
 
 // Matches with "/api/books/:id"
 router
   .route("/:id")
-  .get(booksController.findById)
-  .put(booksController.update)
-  .delete(booksController.remove);
+  .get(usersController.findById)
+  .put(usersController.update)
+  .delete(usersController.remove);
 
 module.exports = router;
