e5b79d9ce4b3601a2cf2f1a87d20ad77eef188c7
Oyuka <58796152+Oyuka100@users.noreply.github.com>
2020-07-18 13:33:24 -0400

    map

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import GoogleMap from "google-map-react";
 import { Icon } from "@iconify/react";
 import locationIcon from "@iconify/icons-mdi/map-marker";
-import {  Card } from "react-bootstrap";
-import '../../styles/map.css';
+import { Card } from "react-bootstrap";
+import "../../styles/map.css";
+import axios from "axios";
 
 const LocationPin = ({ text }) => (
-    <div>
-        <Icon icon={locationIcon} />
-        <p>{text}</p>
-    </div>
+  <div>
+    <Icon icon={locationIcon} />
+    <p>{text}</p>
+  </div>
 );
-const Map = ({ location, zoomLevel }) => (
+const Map = ({ city, zoomLevel }) => {
+  const [location, setLocation] = useState({
+    address: "",
+    location: { lat: 0, lng: 0 },
+  });
+  useEffect(() => {
+    axios
+      .get(
+        "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY&address=" +
+          city
+      )
+      .then((res) => {
+        setLocation({
+          address: res.data.results[0].formatted_address,
+          location: res.data.results[0].geometry.location,
+        });
+      })
+      .catch();
+  }, [city]);
+
+  return (
     <div>
-        <div>
-            <Card className="maps">
-                <Card.Header>MAP</Card.Header>
-                <Card.Body>
-                    <GoogleMap className="map"
-                        style={{ size: "100px" }}
+      <div>
+        <Card className="maps">
+          <Card.Header>MAP</Card.Header>
+          <Card.Body>
+            <GoogleMap
+              className="map"
+              style={{ size: "100px" }}
+              bootstrapURLKeys={{
+                key: "AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY",
+              }}
+              center={location.location}
+              defaultZoom={zoomLevel}
+            >
+              <LocationPin
+                lat={location.location.lat}
+                lng={location.location.lng}
+              />
+            </GoogleMap>
+          </Card.Body>
+        </Card>
+        {/* <GoogleMap
+                        style={{size: "10px"}}
                         bootstrapURLKeys={{ key: "AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY" }}
                         defaultCenter={location}
                         defaultZoom={zoomLevel}
@@ -28,22 +65,9 @@ const Map = ({ location, zoomLevel }) => (
                             lng={location.lng}
                             text={location.address}
                         />
-                    </GoogleMap>
-                </Card.Body>
-            </Card>
-            {/* <GoogleMap
-                style={{size: "10px"}}
-                bootstrapURLKeys={{ key: "AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY" }}
-                defaultCenter={location}
-                defaultZoom={zoomLevel}
-            >
-                <LocationPin
-                    lat={location.lat}
-                    lng={location.lng}
-                    text={location.address}
-                />
-            </GoogleMap> */}
-        </div>
+                    </GoogleMap> */}
+      </div>
     </div>
-);
+  );
+};
 export default Map;
