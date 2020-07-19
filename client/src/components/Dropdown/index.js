78402f4b6333c7ee633015ab2b8e6af90e705c48
Josh Boyd <joshboyd@Joshs-MacBook-Pro.local>
2020-07-18 21:01:28 -0400

    Lada updates

--- client/src/components/Dropdown/index.js
+++ client/src/components/Dropdown/index.js
@@ -1,53 +1,38 @@
 import React from "react";
 //import ReactDOM from "react-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './style.css';
-
-
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./style.css";
 
 class Dropdown extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: 'Basketball' };
-
-    this.handleChange = this.handleChange.bind(this);
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { value: "Basketball" };
   }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value },() => {
-      this.props.switchRoom(this.state.value)
-      this.props.switchSport(this.state.value)
-    });
-  }
-
-  // handleSubmit(event) {
-  //   this.setState({ value: event.target.value },() => {
-  //     this.props.switchRoom(this.state.value)
-  //     // this.props.switchSport(this.state.value)
-  //   });
-  // }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="Basketball" >Basketball</option>
+          <select
+            value={this.state.value}
+            onChange={this.props.handleDropDownChange}
+          >
+            <option value="Basketball">Basketball</option>
             <option value="Soccer">Soccer</option>
             <option value="Tennis">Tennis</option>
           </select>
         </label>
         <label>
-          <select value={this.state.value} onChange={this.handleChange}>
-            <option value="Arlington">Arlington</option>
-            <option value="FairFax">Fairfax</option>
-            <option value="Falls Church">Falls Church</option>
-            <option value="Tysons">Tysons</option>
-
+          <select
+            onChange={this.props.handleDropDownChange}
+          >
+            <option value="Arlington, VA">Arlington</option>
+            <option value="FairFax, VA">Fairfax</option>
+            <option value="Falls Church, VA">Falls Church</option>
+            <option value="Tysons, VA">Tysons</option>
           </select>
-        </label><input type="submit" value="Submit" />
+        </label>
+        <input type="submit" value="Submit" />
       </form>
     );
   }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,174 +1,169 @@
-import React from 'react';
-import '../styles/Home.css';
-import Drop from '../components/Dropdown';
-import Nav from '../components/Navbar';
-import Chat from './chat/chat';
-import io from 'socket.io-client';
-import config from './chat/config'
+import React from "react";
+import "../styles/Home.css";
+import Drop from "../components/Dropdown";
+import Nav from "../components/Navbar";
+import Chat from "./chat/chat";
+import io from "socket.io-client";
+import config from "./chat/config";
 import { Jumbotron, Card, Container, Row, Col } from "react-bootstrap";
 import Map from "../components/Map";
-
-
-
-const location = {
-  address: "1600 Amphitheatre Parkway, Mountain View, california.",
-  lat: 37.42216,
-  lng: -122.08427,
-};
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
       chat: [],
       socket: {},
-      sport: '-',
-      room: '-',
+      room: "Arlington, VA",
     };
   }
 
-  componentDidMount() {
-
-    this.setState({
-      socket: io(config[process.env.NODE_ENV].endpoint)
-    }, () => {
-      this.state.socket.connect()
-
-      // load last 10 messages on page
-      this.state.socket.on('init', (msg) => {
-        this.setState((state) => ({
-          chat: [...state.chat, ...msg.reverse()],
-        }), this.scrollToBottom);
-      });
-
-      // Update the chat if a new message is broadcasted
-      this.state.socket.on('push', (msg) => {
-        this.setState((state) => ({
-          chat: [...state.chat, msg],
-        }), this.scrollToBottom);
-      });
-
-      this.state.socket.on('connect', () => {
-        this.state.socket.emit('sport', this.state.sport)
-        this.state.socket.emit('room', this.state.room)
-        console.log('connected');
-      });
-
-      this.state.socket.on('message', (data) => {
-        console.log('incoming message: ', data)
-        this.setState((state) => {
-
-          // update page w/current message remove from type area
-          return {
-            chat: [...state.chat, data],
-            content: '',
-          };
-        }, this.scrollToBottom);
-      });
-
+  handleDropDownChange = (event) => {
+    this.setState({ room: event.target.value }, () => {
+      this.switchRoom(this.state.value);
     });
+  };
+
+  componentDidMount() {
+    this.setState(
+      {
+        socket: io(config[process.env.NODE_ENV].endpoint),
+      },
+      () => {
+        this.state.socket.connect();
+
+        // load last 10 messages on page
+        this.state.socket.on("init", (msg) => {
+          this.setState(
+            (state) => ({
+              chat: [...state.chat, ...msg.reverse()],
+            }),
+            this.scrollToBottom
+          );
+        });
+
+        // Update the chat if a new message is broadcasted
+        this.state.socket.on("push", (msg) => {
+          this.setState(
+            (state) => ({
+              chat: [...state.chat, msg],
+            }),
+            this.scrollToBottom
+          );
+        });
+
+        this.state.socket.on("connect", () => {
+          this.state.socket.emit("room", this.state.room);
+          console.log("connected");
+        });
+
+        this.state.socket.on("message", (data) => {
+          console.log("incoming message: ", data);
+          this.setState((state) => {
+            // update page w/current message remove from type area
+            return {
+              chat: [...state.chat, data],
+              content: "",
+            };
+          }, this.scrollToBottom);
+        });
+      }
+    );
   }
 
-  switchSport(sport) {
-    this.setState({
-      sport: sport,
-      // room: room,
-      chat: []
-    }, () => {
-      this.state.socket.emit('sport', this.state.sport)
-      console.log('connected', this.state.sport);
-    })
-  }
-
-  switchRoom(room, sport) {
-    this.setState({
-      sport: sport,
-      room: room,
-      chat: []
-    }, () => {
-      this.state.socket.emit('room', this.state.room)
-      console.log('connected', this.state.room);
-    })
+  switchRoom(room) {
+    this.setState(
+      {
+        room: room,
+        chat: [],
+      },
+      () => {
+        this.state.socket.emit("room", this.state.room);
+        console.log("connected", this.state.room);
+      }
+    );
   }
 
   render() {
     return (
-      <Container fluid true>
-        <div className="homepage">
-          <Nav />
-          <Jumbotron>
-            <h1>Welcome to GameOn!</h1>
-          </Jumbotron></div>
-
+      <Container fluid={true}>
+        <Row>
+          <Col>
+            <div className="homepage">
+              <Nav />
+              <Jumbotron>
+                <h1>Welcome to GameOn!</h1>
+              </Jumbotron>
+            </div>
+          </Col>
+        </Row>
         <Row>
           <Col lg>
             <Card className="cd">
               <Card.Header>Select a Sport</Card.Header>
               <Card.Body>
-                <Drop switchRoom={this.switchRoom.bind(this)} switchSport={this.switchSport.bind(this)}/>
+                <Drop
+                  switchRoom={this.switchRoom.bind(this)}
+                  handleDropDownChange={this.handleDropDownChange}
+                />
               </Card.Body>
             </Card>
           </Col>
 
-          <Col xs={12} md={8}>
+          <Col lg>
             <Card className="message">
               <Card.Header>Get Connected</Card.Header>
               <Card.Body>
-                <Chat socket={this.state.socket} chat={this.state.chat} room={this.state.room} sport={this.state.sport}/>
+                <Chat
+                  socket={this.state.socket}
+                  chat={this.state.chat}
+                  room={this.state.room}
+                />
               </Card.Body>
             </Card>
           </Col>
-
         </Row>
 
         <Row>
           <Col>
             <Card className="Google">
-              <Map location={location} zoomLevel={17} />
+              <Map city={this.state.room} zoomLevel={17} />
             </Card>
           </Col>
-
         </Row>
-
-      </Container >
+      </Container>
     );
   }
-  }
+}
 
+// <div className="homepage">
+//   <Nav />
+//   <Jumbotron>
+//     <h1>Welcome to GameOn!</h1>
+//   </Jumbotron>
 
-  // <div className="homepage">
-  //   <Nav />
-  //   <Jumbotron>
-  //     <h1>Welcome to GameOn!</h1>
-  //   </Jumbotron>
+//   <Card className="cd">
+//     <Card.Header>Select a Sport</Card.Header>
+//     <Card.Body>
+//       <Drop />
+//     </Card.Body>
+//   </Card>
 
+//   <h2>
+//     <Card className="message">
+//       <Card.Header>Get Connected</Card.Header>
+//       <Card.Body>
+//         <Chat />
+//       </Card.Body>
+//     </Card>
+//   </h2>
 
-  //   <Card className="cd">
-  //     <Card.Header>Select a Sport</Card.Header>
-  //     <Card.Body>
-  //       <Drop />
-  //     </Card.Body>
-  //   </Card>
+//   <h3>
+//     <Card className="Google">
+//       <Map location={location} zoomLevel={17} />
+//     </Card>
 
-  //   <h2>
-  //     <Card className="message">
-  //       <Card.Header>Get Connected</Card.Header>
-  //       <Card.Body>
-  //         <Chat />
-  //       </Card.Body>
-  //     </Card>
-  //   </h2>
+//   </h3>
 
-  //   <h3>
-  //     <Card className="Google">
-  //       <Map location={location} zoomLevel={17} />
-  //     </Card>
-
-  //   </h3>
-
-
-
-
-  export default Home;
+export default Home;
