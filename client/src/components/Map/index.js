b801794aed54df04cfe48dddf332a4e31a16a735
Hailey Bae <haejinbae@gmail.com>
2020-07-19 21:54:02 -0400

    made modifications to the location pin

--- client/src/components/Map/index.js
+++ client/src/components/Map/index.js
@@ -6,23 +6,44 @@ import { Card } from "react-bootstrap";
 import "../../styles/map.css";
 import axios from "axios";
 
+// //test
+// var service = new google.maps.places.PlacesService(map);
+// var request = {
+//   location: new google.maps.LatLng(-33.8736510, 151.20688960),
+//   radius: 500
+// };
+// service.search(request, function(results) {
+//   console.log(results.length);
+//   for (var i=0; i< results.length; i++) {
+//     console.log(results[i].name, results[i].types)
+//   }
+// })
+
+
+
 const LocationPin = ({ text }) => (
   <div>
     <Icon icon={locationIcon} />
     <p>{text}</p>
   </div>
 );
-const Map = ({ city, zoomLevel }) => {
+
+
+const Map = ({ sportAndCity, zoomLevel }) => {
   const [location, setLocation] = useState({
     address: "",
     location: { lat: 0, lng: 0 },
   });
+  const [sportLocations, setSportLocations] = useState([]);
+
   useEffect(() => {
-    if (city) {
-      axios
+    if (sportAndCity) {
+      const city = sportAndCity.split('-')[1];
+      const sport = sportAndCity.split('-')[0];
+  
+        axios
         .get(
-          "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY&address=" +
-            city
+          `https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY&address=${city}`
         )
         .then((res) => {
           setLocation({
@@ -31,8 +52,18 @@ const Map = ({ city, zoomLevel }) => {
           });
         })
         .catch();
+
+      axios
+      .get(
+        `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${sport}+courts+in+${city}&key=AIzaSyA5Kk4KZ5Ia-PHpXo97abm9JPZYm6xGVgY`
+      )
+      .then((res) => {
+        setSportLocations(res.data.results);
+      })
+      .catch();
     }
-  }, [city]);
+  }, [sportAndCity]);
+
 
   return (
     <div>
@@ -49,10 +80,16 @@ const Map = ({ city, zoomLevel }) => {
               center={location.location}
               defaultZoom={zoomLevel}
             >
-              <LocationPin
+              {/*<LocationPin
                 lat={location.location.lat}
                 lng={location.location.lng}
+              />*/}
+              {sportLocations.map(location => {
+                return <LocationPin
+                lat={location.geometry.location.lat}
+                lng={location.geometry.location.lng} text={location.name}
               />
+              })}
             </GoogleMap>
           </Card.Body>
         </Card>
